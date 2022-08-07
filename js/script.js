b93a986c5633ef3862e436d3106191c68723477c
joaoAugustoParaty <joaodrow@gmail.com>
2022-08-07 17:54:02 -0300

    fist test

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-var wrapper = document.querySelector('.elementor-form-fields-wrapper'),
-form = wrapper.querySelectorAll('#indicou'),
-submitInput = form[0].querySelector('input[type="submit"]');
+function fazPost(url, body){
+console.log("Body=", body)
+let request = new XMLHttpRequest()
+request.open("POST", url, true)
+request.setRequestHeader("Content-type", "application/json")
+request.send(JSON.stringify (body))
 
-function getDataForm(){
-    url = 'https://webhook.site/#!/45ba7968-42c2-4979-a24c-92cd607a876d'
+request.onload = function(){
+    console.log(this.responseText)
 }
+return request.responseText
+}
+function cadastroUsuario(){
+    event.preventDefault()
+    let url = "https://webhook.site/45ba7968-42c2-4979-a24c-92cd607a876d"
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    console.log(nome)
+    console.log(email)
 
+    body = { 
+        "name": nome,
+        "email": email
+    }
+
+    fazPost(url, body)
+
+}
