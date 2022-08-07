782af1c1f781767e518a86ceac6e32a495d309d7
joaoAugustoParaty <joaodrow@gmail.com>
2022-08-07 14:46:43 -0300

    teste1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var wrapper = document.querySelector('.elementor-form-fields-wrapper'),
+form = wrapper.querySelectorAll('#indicou'),
+submitInput = form[0].querySelector('input[type="submit"]');
+
+function getDataForm(){
+    url = 'https://webhook.site/#!/45ba7968-42c2-4979-a24c-92cd607a876d'
+}
+
